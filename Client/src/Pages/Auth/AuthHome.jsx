599b0621ee42bd4f3f40cbe1aaab0f696ac22136
atullya <atulmzn1@gmail.com>
2024-12-31 21:41:35 +0545

    new

--- Client/src/Pages/Auth/AuthHome.jsx
+++ Client/src/Pages/Auth/AuthHome.jsx
@@ -24,6 +24,7 @@ const AuthHome = () => {
     } catch (error) {
       if (error.response && error.response.status === 401) {
         // Handle Unauthorized error (JWT not valid or missing)
+        // navigate("/authhome");
         navigate("/authhome");
       } else {
         console.error("Error fetching user data:", error);
